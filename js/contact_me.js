c5860bf5525d5829409cd4ec557fada550e9fc2e
nanduanil <nanduanil@gmail.com>
2014-11-30 18:20:12 +0530

    Enable jquery validation of form. Enable mail functionality Include Country Field in Contact Page

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,11 +9,11 @@ $(document).ready(function() {
 
 /*
   Jquery Validation using jqBootstrapValidation
-   example is taken from jqBootstrapValidation docs 
+   example is taken from jqBootstrapValidation docs  */
  
 $(function() {
 
-    $("input,textarea").jqBootstrapValidation({
+    $("input,select,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // something to have when submit produces an error ?
@@ -47,7 +47,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent. Our doctor will get back to you soon. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -77,7 +77,7 @@ $(function() {
     });
 });
 
- */
+
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
     $('#success').html('');
